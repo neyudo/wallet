ebeb51d53ed78a548550dc78b2a5e7bf4a7063e0
Jonathan <magno3000@gmail.com>
2021-10-26 14:18:48 +0200

    26102021Aplpha

--- billetera_7.js
+++ billetera_7.js
@@ -2,7 +2,6 @@
 const search = document.getElementById('search')
 const matchList = document.getElementById('match-list')
 const api_url = 'https://api1.binance.com/api/v3/ticker/price'
-const api_SoloPrecio = 'https://api1.binance.com/api/v3/avgPrice?symbol='
 
 const addCosas = [];
 //Precios ya establecidos BTC ETH
@@ -10,17 +9,9 @@ const addCosas = [];
 //____________________________
 
 //Introduce con un click tu Crypto que quieres observar
-const add = async () => {
-    let ident = event.target.id.split(/("")/u);
-    /* let ident = event.target.id.split(/("")/u);
+const add = () => {
+    let ident = event.target.id;
     console.log(ident);
-    UI.addCryptoToList(ident);
-    matches = [];
-    matchList.innerHTML = ''; */
-    /* const responseP = await fetch(api_SoloPrecio + ident);
-    const datosSoloPrecio2 = await responseP.json();
-    console.log(datosSoloPrecio2); */
-    
     fetch(api_url)
         .then(response => response.json())
         .then(binancePrice => {
@@ -28,10 +19,10 @@ const add = async () => {
             let nuDeIn = binancePrice.findIndex((objeto, indice, cosas) => {
                 if (objeto.symbol == ident) {
                     const arr = objeto;
-                    console.log(arr);
+                    console.log(arr.price);
                     UI.addCryptoToList(arr);
                 }
-            }); 
+            });
             matchList.innerHTML = '';
 
         });
@@ -45,6 +36,8 @@ const buscadatos = async  precios => {
         const regex = new RegExp(`^${precios}`, 'gi');
         return dato.symbol.match(regex) /*|| dato.price.match(regex)*/;
     });
+/*     console.log(matches);
+ */
     if(precios.length === 0 )
     {
         matches = [];
@@ -81,121 +74,91 @@ const outputHtml = matches => {
  class UI {
     static displayCrypto() {
         //Brocker Binance
-        /* const cryptos = ['BTCUSDT', 'ETHUSDT', 'KSMUSDT'];
-        cryptos.forEach((crypto) => UI.addCryptoToList(crypto.split(/("")/u))); */
-        //
         const pri = async () => {
+            const live = document.getElementById('crypto-list');
+
             const response = await fetch(api_url),
-                datos = await response.json(),
-                btc = datos[11],
-                eth = datos[12],
-                ksm = datos[1007],
-                cryptos = [btc, eth, ksm],
-                tocri = [{
-                    symbol: 'btc',
-                    price: '65000'
-                },
-                {
-                    symbol: 'eth',
-                    price: '4500'
-                }
-                ];
-                            
-            cryptos.forEach((crypto) => UI.addCryptoToList(crypto, cryptos, tocri));
-        }
+            datos = await response.json(),
+            btc = datos[11],
+            eth = datos[12],
+            ksm = datos[1007],
+            cryptos = [btc, eth, ksm];
+            //console.log(cryptos);
+            cryptos.forEach((crypto) => UI.addCryptoToList(crypto));
+        }    
         pri();
     }
+    
+     static addCryptoToList(crypto) {
+         const list = document.querySelector('#crypto-list');
+         const row = document.createElement('tr');
+         row.setAttribute("id", "otro")
+         list.appendChild(row);
+                 row.innerHTML = `
+                <td class="sim" value="sip" id="simbolo">${crypto.symbol}</td>
+                <td class="precio">${parseFloat(crypto.price)}</td>
+                <td>0</td>
+                <td>0</td>
+                <td><a href="#" class="btn btn-danger btn-sm delete" >X</a></td>
+            `;
+         const clickPrecio = document.getElementsByClassName('sim');
+         //console.log(clickPrecio);
+         const pri = async () => {
+             const pux = [];
+             const listaDePrecios = [];
+             //esta es la constante con la clase del precio dentro del row.innerHTML
+             const live = document.getElementsByClassName('precio'),
+                 response = await fetch(api_url),
+                 datos = await response.json();
+             //constante que me deja entrar a la clase del td y me permite ver su contenido con unas iteraciones
+             // Itero en la variante ClickPrecio y obtengo su informacion en este caso los simblolos del cripto deseado
+             for (let cell of clickPrecio) {
+                 let val = cell.innerText.split(/("")/u);
+                 let compa = datos.findIndex((objeto, indice, cosa) => {
+                 if (objeto.symbol == val) {
+                     const resul = objeto;
+                     listaDePrecios.push(resul);
+                     }
+             });
+                 
+             }
+             
+             for (let [i, cripi] of (listaDePrecios).entries()) {
+                            live[i].innerHTML = `${parseFloat(cripi.price)}`;
+                            
+       
+                        }
+                }
+                setInterval(() => {
+                    pri();
+                }, 10000);
+         /* var table = document.getElementById('crypto-list');
+         const flu = table.rows;
+         console.log(flu);
+         for (let row of table.rows) {
+             for (let cell of row.cells) {
+                 console.log(cell.innerText.split(/("")/u))
+             }
+         } */
+     }
 
-     static addCryptoToList(crypto, cryptos, mas) {
-        //console.log(cryptos);
-        
-        //const clickPrecio = document.getElementsByClassName('sim');
-        //console.log(clickPrecio);
-        const listaDePrecios1 = [];
-
-        //const clickPrecio = document.getElementsByClassName('sim');
-
-        const flag = async picho => {
-            for (let cell of crypto.symbol.split(/("")/u)) {
-                //let val = cell.innerText.split(/("")/u);
-                const response2 = await fetch(api_SoloPrecio + cell);
-                const datosSoloPrecio = await response2.json();
-                //console.log(datosSoloPrecio);
-                listaDePrecios1.push(datosSoloPrecio);
-                UI.addCryptoToListF(crypto, datosSoloPrecio, mas)
-                // const map = new Map(Object.entries(datosSoloPrecio));
-                // console.log(map);
-                // console.log(Object.entries(datosSoloPrecio));
-                // UI.addCryptoToListF(crypto, datosSoloPrecio);
-                //console.log(listaDePrecios1);
-                //document.getElementById('pre').textContent = listaDePrecios1[0].price;
-                
-            }
-        }
-        flag();        
-        /* for (let [i, cripi] of (listaDePrecios1).entries()) {
-            live[i].innerHTML = `${parseFloat(cripi.price)}`;
-
-
-        } */
-        
-
-
-        
-        
-    }
-    static addCryptoToListF(cryptoF, algo, mas) {
-        console.log(cryptoF);
-        //console.log(algo);
-        //const map = new Map(Object.entries(algo));
-        console.log(mas);
-        //console.log(map);
-        const live = document.getElementsByClassName('precio');
-        /* const pus = pux => {
-            live.innerHTML = `${algo.price}`;
-            
-        };
-        pus(); */
-        for (let [i, cripi] of [mas].entries()) {
-            console.log(cripi.price);
-            console.log(i);
-            live[i].innerHTML = `${parseFloat(cripi.price)}`;
-            
-
-        }
-         
-        
-        const list = document.querySelector('#crypto-list');
-        const row = document.createElement('tr');
-        row.setAttribute("id", "otro");
-        list.appendChild(row);
-         row.innerHTML = `
-                               <td class="sim" onload="mifuc()" >${cryptoF.symbol}</td>
-                               <td class="precio" id="pre">0</td>
-                               <td>0</td>
-                               <td>0</td>
-                               <td><a href="#" class="btn btn-danger btn-sm delete" >X</a></td>
-                           `;
-    }
+     
 
      static deleteCrypto(el) {
         if (el.classList.contains('delete')) {
             el.parentElement.parentElement.remove();
             
         }
-     }
-     
-}
+    }
+ }
 //Delete
 document.querySelector('#crypto-list').addEventListener('click', function cosi(event) {
     UI.deleteCrypto(event.target);
 });
 
 // Event: Display Crypto
+
 document.addEventListener('DOMContentLoaded', UI.displayCrypto);
 //Event: Add
 
 search.addEventListener('input', () => buscadatos(search.value));
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-});
