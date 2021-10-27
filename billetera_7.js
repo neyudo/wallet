1a289ed30e5fa06327cc0219d70bb2ef33add9c8
Jonathan <magno3000@gmail.com>
2021-10-27 13:33:04 +0200

    27102021AlphaFaltaRemoveDelLocal

--- billetera_7.js
+++ billetera_7.js
@@ -2,13 +2,11 @@
 const search = document.getElementById('search')
 const matchList = document.getElementById('match-list')
 const api_url = 'https://api1.binance.com/api/v3/ticker/price'
-
-const addCosas = [];
 //Precios ya establecidos BTC ETH
 /*  */
 //____________________________
 
-//Introduce con un click tu Crypto que quieres observar
+// EVENT Add Crypto Introduce con un click tu Crypto que quieres observar
 const add = () => {
     let ident = event.target.id;
     console.log(ident);
@@ -21,6 +19,10 @@ const add = () => {
                     const arr = objeto;
                     console.log(arr.price);
                     UI.addCryptoToList(arr);
+                    //Add crypto to Local
+                    CryptoList.addCrypto(arr);
+                    //alert('Nice! your Crypto is ther');
+                    UI.showAlert('Nice! Crypto Added', 'success');
                 }
                 
             });
@@ -69,34 +71,36 @@ const outputHtml = matches => {
 
  // representa una moneda cripto
  class Crypto {
-    constructor(objeto) {
-        this.objeto = objeto;
+     constructor(objeto) {
+         this.objeto = objeto;
      }
  }
- class UI {
-    static displayCrypto() {
-        //Brocker Binance
-        const pri = async () => {
-            const live = document.getElementById('crypto-list');
 
-            const response = await fetch(api_url),
-            datos = await response.json(),
-            btc = datos[11],
-            eth = datos[12],
-            ksm = datos[1007],
-            cryptos = [btc, eth, ksm];
-            //console.log(cryptos);
-            cryptos.forEach((crypto) => UI.addCryptoToList(crypto));
-        }    
-        pri();
-    }
-    
+ //Class UI
+ class UI {
+     static displayCrypto() {
+         //Brocker Binance
+         const pri = async () => {
+             const live = document.getElementById('crypto-list');
+
+             const response = await fetch(api_url),
+                 datos = await response.json(),
+                 btc = datos[11],
+                 eth = datos[12],
+                 ksm = datos[1007],
+                 cryptos = [btc, eth, ksm];
+             //console.log(cryptos);
+             cryptos.forEach((crypto) => UI.addCryptoToList(crypto));
+         }
+         pri();
+     }
+
      static addCryptoToList(crypto) {
          const list = document.querySelector('#crypto-list');
          const row = document.createElement('tr');
          row.setAttribute("id", "otro")
          list.appendChild(row);
-                 row.innerHTML = `
+         row.innerHTML = `
                 <td class="sim" value="sip" id="simbolo">${crypto.symbol}</td>
                 <td class="precio">${parseFloat(crypto.price)}</td>
                 <td>0</td>
@@ -117,23 +121,23 @@ const outputHtml = matches => {
              for (let cell of clickPrecio) {
                  let val = cell.innerText.split(/("")/u);
                  let compa = datos.findIndex((objeto, indice, cosa) => {
-                 if (objeto.symbol == val) {
-                     const resul = objeto;
-                     listaDePrecios.push(resul);
+                     if (objeto.symbol == val) {
+                         const resul = objeto;
+                         listaDePrecios.push(resul);
                      }
-             });
-                 
+                 });
+
              }
-             
+
              for (let [i, cripi] of (listaDePrecios).entries()) {
-                            live[i].innerHTML = `${parseFloat(cripi.price)}`;
-                            
-       
-                        }
-                }
-                setInterval(() => {
-                    pri();
-                }, 10000);
+                 live[i].innerHTML = `${parseFloat(cripi.price)}`;
+
+
+             }
+         }
+         setInterval(() => {
+             pri();
+         }, 10000);
          /* var table = document.getElementById('crypto-list');
          const flu = table.rows;
          console.log(flu);
@@ -147,20 +151,71 @@ const outputHtml = matches => {
          document.querySelector('#search').value = '';
      }
 
-     
+
 
      static deleteCrypto(el) {
-        if (el.classList.contains('delete')) {
-            el.parentElement.parentElement.remove();
-            
+         if (el.classList.contains('delete')) {
+             el.parentElement.parentElement.remove();
+             //Remove alert
+             UI.showAlert('Crypto Removed', 'success');
+
+         }
+     }
+
+     static showAlert(message, className) {
+         const div = document.createElement('div');
+         div.className = `alert alert-${className}`;
+         div.appendChild(document.createTextNode(message));
+         const container = document.querySelector('.col-md-6');
+         const form = document.querySelector('#alerta');
+         container.insertBefore(div, form)
+         //Vanish in 2 seconds
+         setTimeout(() => {
+             document.querySelector('.alert').remove();
+         }, 2000);
+     }
+
+ }
+
+class CryptoList {
+    static getCrypto() {
+        let cryptoMonedas;
+        if (localStorage.getItem('cryptoMonedas') === null) {
+            cryptoMonedas = [];
+        } else {
+            cryptoMonedas = JSON.parse(localStorage.getItem('cryptoMonedas'));
         }
+        return cryptoMonedas;
     }
+
+    static addCrypto(crypto) {
+        const cryptoMonedas = CryptoList.getCrypto();
+        cryptoMonedas.push(crypto);
+        localStorage.setItem('cryptoMonedas', JSON.stringify(cryptoMonedas))
+    }
+
+    static removeCrypto(symbol) {
+        const cryptoMonedas = CryptoList.getCrypto();
+        cryptoMonedas.forEach((crypto, index) => {
+            console.log(crypto.symbol);
+            console.log(symbol);
+            if (crypto.symbol === symbol) {
+                cryptoMonedas.split(index, 1);
+            }
+        });
+    }
+     
  }
 //Delete
 document.querySelector('#crypto-list').addEventListener('click', function cosi(event) {
+    //Remove a Crypto from UI
     UI.deleteCrypto(event.target);
+    //Remove Crypto from Local
+    console.log(event.target.parentElement.previousElementSibling.textContent);
+    console.log(event.path[2].textContent);
+    console.log(event.path[2]);
+    CryptoList.removeCrypto(event.path[2].textContent);
 });
-
 // Event: Display Crypto
 
 document.addEventListener('DOMContentLoaded', UI.displayCrypto);
